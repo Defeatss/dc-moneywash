2f413b856a86405141c1b6abf8c0b54493b70701
Shay BBX <107814631+ShayBBX1@users.noreply.github.com>
2022-11-05 14:29:26 +0100

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,81 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- ########## Ped Coordinates ########## --
+
+local hiddenped = {["coords"] = {
+    [1] = vector4(1241.7, -3274.39, 5.53, 289.95)
+    -- [1] = vector4(1240.93, -3260.28, 5.53, 298.6),
+    -- [2] = vector4(1241.7, -3274.39, 5.53, 289.95)
+}}
+
+-- ########## Functions ########## --
+
+local function getMarkedBillWorth()
+    local Player = QBCore.Functions.GetPlayer(source)
+    local markedbilltotal = 0
+    for k, v in pairs(Player.PlayerData.items) do
+        if v.name == "markedbills" then
+            markedbilltotal = markedbilltotal + (v.info.worth * v.amount)
+            Player.Functions.RemoveItem("markedbills", v.amount, false)
+        end
+    end
+    info = {
+        worth = markedbilltotal
+    }
+    Player.Functions.AddItem("markedbills", 1 , false ,info)
+    return markedbilltotal
+end
+
+local function WashMarkedBills(balance, price)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local newworth = balance - price
+    local netprice = math.floor(price * Config.Percentage)
+    if newworth > 0 then
+        info = {
+            worth = newworth
+        }
+        Player.Functions.RemoveItem("markedbills", 1, false)
+        Player.Functions.AddItem("markedbills", 1 , false ,info)
+
+        Player.Functions.AddMoney("cash",netprice)
+        TriggerClientEvent('QBCore:Notify', source, "Washed $"..price.." black money and got $"..netprice.." back!")
+    else
+        Player.Functions.RemoveItem("markedbills", 1, false)
+        Player.Functions.AddMoney("cash",netprice)
+    end
+end
+
+-- ########## Callbacks ########## --
+
+QBCore.Functions.CreateCallback('nc-moneywash:server:pedcoords', function(source, cb)
+    local r = math.random(1,#hiddenped["coords"])
+    for _,v in pairs(hiddenped) do
+        pedloc = v[r]
+    end
+    cb(pedloc)
+end)
+
+-- ########## Events ########## --
+
+RegisterNetEvent("nc-moneywash:server:sellthem",function(amount)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local TotalAmount = tonumber(amount)
+    if Config.BlackMoneyName == "markedbills" then
+        Balance = getMarkedBillWorth()
+    else
+        Balance = Player.Functions.GetItemByName(Config.BlackMoneyName).amount
+    end
+    if Balance >= TotalAmount then
+        if Config.BlackMoneyName == "markedbills" then
+            WashMarkedBills(Balance,TotalAmount)
+        else
+			local netprice = math.floor(amount * Config.Percentage)
+            if Player.Functions.RemoveItem(Config.BlackMoneyName, TotalAmount) then
+                Player.Functions.AddMoney("cash",netprice)
+                TriggerClientEvent('QBCore:Notify', source, "Washed $"..amount.." black money and got $"..netprice.." back!")
+            end
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', source, "You don't have that much blackmoney on you!", 'error')
+    end
+end)
