ed24c68ef088ae8713a64b96057c41e9cbe1c5c3
Shay BBX <107814631+ShayBBX1@users.noreply.github.com>
2022-11-05 14:28:02 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+author "Shay BBX#1918"
+version "V1.1"
+description "Money Washing Script"
+game "gta5"
+
+shared_scripts {'config.lua'}
+client_scripts {'client.lua'}
+server_scripts {'server.lua'}
